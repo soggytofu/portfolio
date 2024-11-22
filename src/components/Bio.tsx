717525ad9fe839d25b7b9e3249673aed730ebdae
soggytofu <regispak@gmail.com>
2024-11-22 14:57:30 -0800

    updated bio

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -5,15 +5,7 @@ export function Bio() {
         <h2 className="text-3xl font-bold mb-6 text-center">About Me</h2>
         <div className="bg-gray-700 p-8 rounded-lg overflow-hidden shadow-lg drop-shadow-xl">
           <p className="text-lg leading-relaxed text-center">
-          Hi, I'm Regis Pak. I'm a senior at UCSC studying Computer Science Game Design, 
-          I've been playing games my entire life, but really took my first steps into 
-          game design with a course during my senior year of high school. It taught some 
-          basics, and I felt new aspirations into this field and started continuing it 
-          in college. I mainly make games within Unity 3d, but I've started learning Unreal. 
-          Here are some of the projects I've made throughout University. This Itch is just to hold 
-          me over until I can finish learning HTML / CSS to make a website I like. 
-          Thanks for stopping by.
-
+          Test Bio Cool beans
           </p>
         </div>
       </div>
